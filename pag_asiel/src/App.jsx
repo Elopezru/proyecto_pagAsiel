b00286f0540d02cabb672c17de589ef64bf8730d
Elopezru <quique-969@hotmail.com>
2026-04-15 01:15:04 -0600

    feat: update

--- pag_asiel/src/App.jsx
+++ pag_asiel/src/App.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
-import { ThemeProvider } from '@emotion/react'
-import Button from '@mui/material/Button'
+import React from 'react';
+import { ThemeProvider } from '@emotion/react';
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SendIcon from '@mui/icons-material/Send';
+import DrawerP from './Componentes/DrawerP';
+import Divider from '@mui/material/Divider';
 
 
 
@@ -8,8 +12,17 @@ function App() {
   
   return (
     <div>
-      <Button color="secondary">
+      <DrawerP />
+      <Divider />
+      <Button variant ="contained" color="secondary" endIcon= {<SendIcon />}>
        HELLO 
+      </Button>
+      <p>
+        PAGINA ASIEL
+      </p>
+
+      <Button variant ="outlined" color="primary" disableElevation endIcon= {<DeleteIcon />}>
+       otro 
       </Button>
     </div>
   )
